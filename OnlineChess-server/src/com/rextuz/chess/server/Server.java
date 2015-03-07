eb8530507067fb0f6f373311888d82db17f08f52
Rextuz <rextuz@gmail.com>
2015-03-07 15:48:42 +0300

    added server, added client UI

--- OnlineChess-server/src/com/rextuz/chess/server/Server.java
+++ OnlineChess-server/src/com/rextuz/chess/server/Server.java
@@ -1,6 +1,5 @@
 package com.rextuz.chess.server;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -11,8 +10,10 @@ public class Server extends UnicastRemoteObject implements ServerSend {
 	private static final long serialVersionUID = 1L;
 	private Users users;
 	private Users searching;
+	private static int PORT = 4242;
 
 	public Server() throws RemoteException {
+		super(PORT);
 		users = new Users();
 		searching = new Users();
 	}
@@ -20,13 +21,11 @@ public class Server extends UnicastRemoteObject implements ServerSend {
 	public static void main(String[] args) throws RemoteException {
 		try {
 			Server server = new Server();
-			ServerSend stub = (ServerSend) UnicastRemoteObject.exportObject(
-					server, 0);
-			Registry registry = LocateRegistry.getRegistry();
-			registry.bind("OnlineChess", (Remote) stub);
+			Registry registry = LocateRegistry.createRegistry(PORT);
+			registry.bind("OnlineChess", server);
 			System.out.print("Server is ready");
 		} catch (Exception e) {
-			System.err.print(e);
+			System.err.println(e);
 		}
 	}
 
