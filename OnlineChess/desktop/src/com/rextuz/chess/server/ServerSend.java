b7e1be9bcc65e712396cdb97e5fc45db8903d455
Rextuz <rextuz@gmail.com>
2015-03-17 14:04:35 +0300

    project stucture fix

---
+++ OnlineChess/desktop/src/com/rextuz/chess/server/ServerSend.java
@@ -0,0 +1,17 @@
+package com.rextuz.chess.server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface ServerSend extends Remote {
+
+	public boolean login(String name) throws RemoteException;
+
+	public List<String> find(String name) throws RemoteException;
+
+	public boolean connect(String name) throws RemoteException;
+
+	public boolean disconnect(String name) throws RemoteException;
+
+}
